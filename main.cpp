269a13e6ba342954e0c59293e9ff70cc6b1ad6cf
Leila <leila.naji16@gmail.com>
2019-11-09 20:47:12 +0100

    main

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "Protein.h"
+
+using namespace std;
+
+
+
+int main() {
+	Protein protein;
+	cout<<protein.getProteinDescription()<<endl ;
+	
+	return 0;
+};
