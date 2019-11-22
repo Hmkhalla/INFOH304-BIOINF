97110f3b1098dc9e910c0fb7a8d68d184f9e38ea
Al Shoha Shady <shadysho99@gmail.com>
2019-11-22 08:16:14 +0100

    Redefine protein

--- Read blast/Protein.cpp
+++ Read blast/Protein.cpp
@@ -4,35 +4,41 @@
 #include "Protein.h"
 #include <vector>
 
-Protein::Protein()
-{
-}
 Protein::Protein(string description, vector<uint8_t> sequence)
 {
 	m_proteinDescription = description;
 	m_vectorSequence = sequence;
 }
 
-			    m_proteinDescription=line.substr(1);}
-		    if(line[0]=='>'){
-	    while (getline(queryFlux,line).good()) {
-	    string line;
-	if(queryFlux.is_open()){
+Protein::Protein(char *fasta)
+{
 	ifstream queryFlux(fasta);
-Protein:: Protein( char* fasta) {
-		    else{
-			    m_proteinSequence+=line;}	
+	if (queryFlux.is_open())
+	{
+		string line;
+		while (getline(queryFlux, line).good())
+		{
+			if (line[0] == '>')
+			{
+				m_proteinDescription = line.substr(1);
+			}
+			else
+			{
+				m_proteinSequence += line;
+			}
+		}
 	}
-	else {
-		cout <<"Error: Impossible to open the file." <<endl;}
+	else
+	{
+		cout << "Error: Impossible to open the file." << endl;
 	}
-	return m_vectorSequence;
-	}
+}
 
-Protein::~Protein(){
-vector<uint8_t> const& getVectorSequence(){
-
-	    }
+Protein::~Protein()
+{
+	vector<uint8_t> const &getVectorSequence();
+	{
+	}
 	m_vectorSequence.clear();
 }
 
@@ -41,103 +47,105 @@ string Protein::getProteinDescription()
 	return m_proteinDescription;
 }
 
-/*
-void Protein::convertToValue(){
-	int length=m_proteinSequence.length();
-	for( int i=0; i<length; i++) {
-		char val=m_proteinSequence[i];
-		switch (val) {
-			case '-':
-			    m_vectorSequence.push_back(0);
-			    break;
-			case 'A':
-			    m_vectorSequence.push_back(1);
-			    break;
-			case 'B':
-			    m_vectorSequence.push_back(2);
-			    break;
-			case 'C':
-			    m_vectorSequence.push_back(3); 
-			    break;
-			case 'D':
-			    m_vectorSequence.push_back(4);
-			    break;
-			case 'E':
-			    m_vectorSequence.push_back(5);
-			    break;
-			case 'F':
-			    m_vectorSequence.push_back(6);
-			    break;
-			case 'G':
-			    m_vectorSequence.push_back(7);
-			    break;
-			case 'H':
-			    m_vectorSequence.push_back(8);
-			    break;
-			case 'I':
-			    m_vectorSequence.push_back(9);
-			    break;
-			case 'J':
-			    m_vectorSequence.push_back(27);
-			    break;
-			case 'K':
-			    m_vectorSequence.push_back(10);
-			    break;
-			case 'L':
-			    m_vectorSequence.push_back(11);
-			    break;
-			case 'M':
-			    m_vectorSequence.push_back(12);
-			    break;
-			case 'N':
-			    m_vectorSequence.push_back(13);
-			    break;
-			case 'O':
-			    m_vectorSequence.push_back(26);
-			    break;
-			case 'P':
-			    m_vectorSequence.push_back(14);
-			    break;
-			case 'Q':
-			    m_vectorSequence.push_back(15);
-			    break;
-			case 'R':
-			    m_vectorSequence.push_back(16);
-			    break;
-			case 'S':
-			    m_vectorSequence.push_back(17);
-			    break;
-			case 'T':
-			    m_vectorSequence.push_back(18);
-			    break;
-			case 'U':
-			    m_vectorSequence.push_back(24);
-			    break;
-			case 'V':
-			    m_vectorSequence.push_back(19);
-			    break;
-			case 'W':
-			    m_vectorSequence.push_back(20);
-			    break;
-			case 'X':
+vector<uint8_t> Protein::convertToValue()
+{
+	int length = m_proteinSequence.length();
+	for (int i = 0; i < length; i++)
+	{
+		char val = m_proteinSequence[i];
+		switch (val)
+		{
+		case '-':
+			m_vectorSequence.push_back(0);
+			break;
+		case 'A':
+			m_vectorSequence.push_back(1);
+			break;
+		case 'B':
+			m_vectorSequence.push_back(2);
+			break;
+		case 'C':
+			m_vectorSequence.push_back(3);
+			break;
+		case 'D':
+			m_vectorSequence.push_back(4);
+			break;
+		case 'E':
+			m_vectorSequence.push_back(5);
+			break;
+		case 'F':
+			m_vectorSequence.push_back(6);
+			break;
+		case 'G':
+			m_vectorSequence.push_back(7);
+			break;
+		case 'H':
+			m_vectorSequence.push_back(8);
+			break;
+		case 'I':
+			m_vectorSequence.push_back(9);
+			break;
+		case 'J':
+			m_vectorSequence.push_back(27);
+			break;
+		case 'K':
+			m_vectorSequence.push_back(10);
+			break;
+		case 'L':
+			m_vectorSequence.push_back(11);
+			break;
+		case 'M':
+			m_vectorSequence.push_back(12);
+			break;
+		case 'N':
+			m_vectorSequence.push_back(13);
+			break;
+		case 'O':
+			m_vectorSequence.push_back(26);
+			break;
+		case 'P':
+			m_vectorSequence.push_back(14);
+			break;
+		case 'Q':
+			m_vectorSequence.push_back(15);
+			break;
+		case 'R':
+			m_vectorSequence.push_back(16);
+			break;
+		case 'S':
+			m_vectorSequence.push_back(17);
+			break;
+		case 'T':
+			m_vectorSequence.push_back(18);
+			break;
+		case 'U':
+			m_vectorSequence.push_back(24);
+			break;
+		case 'V':
+			m_vectorSequence.push_back(19);
+			break;
+		case 'W':
+			m_vectorSequence.push_back(20);
+			break;
+		case 'X':
 			m_vectorSequence.push_back(21);
-			    break;
-			case 'Y':
-			    m_vectorSequence.push_back(22);
-			    break;
-			case 'Z':
-			    m_vectorSequence.push_back(23);
-			    break;
-			case '*':
-			    m_vectorSequence.push_back(25);
-			    break;
-			default:
-			    cout<<"Error"<<endl;
-			    break;
-        } 
-    }
-}		 
-	*/
+			break;
+		case 'Y':
+			m_vectorSequence.push_back(22);
+			break;
+		case 'Z':
+			m_vectorSequence.push_back(23);
+			break;
+		case '*':
+			m_vectorSequence.push_back(25);
+			break;
+		default:
+			cout << "Error" << endl;
+			break;
+		}
+	}
+	return m_vectorSequence;
+}
 
 void Protein::printVectorSequence()
 {
--- Read blast/Protein.h
+++ Read blast/Protein.h
@@ -12,18 +12,19 @@ class Protein
 {
 private:
 	string m_proteinDescription;
+	string m_proteinSequence;
 
 public:
 	std::vector<uint8_t> m_vectorSequence;
-	
-	public:
-	Protein(char* fasta);
+
+public:
+	Protein(char *fasta);
 	~Protein();
 	Protein(string m_proteinDescription, vector<uint8_t> m_vectorSequence);
 	Protein(string fasta);
 	string getProteinDescription();
 	string getProteinSequence();
-	void convertToValue();
+	vector<uint8_t> convertToValue();
 	void printVectorSequence();
 	vector<uint8_t> getVectorSequence();
 };
